8ee1409a04ed93413de923933591da9022366676
Nastya@gmail.com <proekt28n@gmail.com>
2025-04-18 23:27:29 +0500

    Task 1

---
+++ Задание 1.py
@@ -0,0 +1,21 @@
+stroka = "I love violet"
+
+n1 = stroka[2]
+n2 = stroka[-2]
+n3 = stroka[:6]
+n4 = stroka[:-2]
+n5 = stroka[0:14:2]
+n6 = stroka[1:14:2]
+n7 = stroka[::-1]
+n8 = stroka[::-2]
+n9 = len(stroka)
+
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
+print(n8)
+print(n9)
