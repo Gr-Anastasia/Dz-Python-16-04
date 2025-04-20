dd9228278df57893f10de061f0166c264b0f464f
Nastya@gmail.com <proekt28n@gmail.com>
2025-04-20 17:16:38 +0500

    Task 6 and fix erorr

--- Задание 5.py
+++ Задание 5.py
@@ -1,7 +1,7 @@
 stroka = "In the hole in the ground there lived a hobbit"
 
-fin = stroka.find("h",0)
-fin2 = stroka.find("h",-8)
+fin = stroka.find("h")
+fin2 = stroka.find("h",-6)
 
 res = stroka[:fin] + stroka[fin2:]
 
---
+++ Задание 6.py
@@ -0,0 +1,7 @@
+stroka = "AaBbCcDd"
+
+a = stroka[1::2]
+b = stroka[0::2]
+
+
+print(a, b)
